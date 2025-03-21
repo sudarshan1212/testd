c67de775b90d2781916094796860f70145dc9c96
Sudarshan <sudarshan@atatus.com>
2025-03-21 14:39:13 +0530

    Added only the frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div style={{ textAlign: "center", padding: "20px" }}>
+            <h1>Test Page</h1>
+            <p>This is a simple test page in Vite.</p>
+        </div>
+    );
+}
+
+export default App;
